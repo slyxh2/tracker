d70c3dd1ed07d97241fde71aa227c8d1cf262ea2
slyxh2 <750772121@qq.com>
2022-09-03 15:06:45 -0400

    finish domErrorReport jsErrorreport function

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,8 @@
 import { DefaultOptons, Options, TrackerConfig } from '../types/index';
 import { createHistoryEvent } from '../utils/pv';
 
+const MouseEventList: string[] = ['click', 'dblclick', 'contextmenu', 'mousedown', 'mouseup', 'mouseenter', 'mouseout', 'mouseover'];
+
 export default class tracker {
     public data: Options;
     constructor(options: Options) {
@@ -24,19 +26,80 @@ export default class tracker {
     public setExtra<T extends DefaultOptons['extra']>(extra: T) {
         this.data.extra = extra;
     }
+    public sendTracker<T>(data: T) {
+        this.reportTracker(data);
+    }
+    private targetKeyReport() {
+        MouseEventList.forEach(event => {
+            window.addEventListener(event, (e) => {
+                const target = e.target as HTMLElement;
+                const targetKey = target.getAttribute('target-key');
+                if (targetKey) {
+                    console.log(targetKey);
+                    this.reportTracker({
+                        event,
+                        targetKey
+                    })
+                }
+
+            })
+        })
+    }
+
+    private errorHandle() {
+        document.addEventListener('error', (event) => {
+            this.reportTracker({
+                event: 'jsError',
+                targetKey: 'message',
+                message: event.message
+            })
+        }, true)
+    }
+
+    private promiseRejectedHandle() {
+        window.addEventListener('unhandledrejection', (event) => {
+            event.promise.catch(error => {
+                this.reportTracker({
+                    event: 'promiseRejected',
+                    targetKey: 'message',
+                    message: error
+                })
+            })
+
+        }, true)
+    }
     private captureEvent<T>(event: string[], targetKey: string, data?: T) {
         event.forEach(ev => {
             window.addEventListener(ev, () => {
-                console.log('ok');
+                this.reportTracker({
+                    ev,
+                    targetKey,
+                    data
+                })
             })
         })
     }
     private installTracker() {
         if (this.data.historyTracker) {
-            this.captureEvent(['pushState', 'replaceState', 'popState'], 'history-pv');
+            this.captureEvent(['pushState', 'replaceState', 'popstate'], 'history-pv');
         }
         if (this.data.hashTracker) {
             this.captureEvent(['hashchange'], 'hash-pv');
         }
+        if (this.data.domTracker) {
+            this.targetKeyReport();
+        }
+        if (this.data.jsError) {
+            this.errorHandle();
+            this.promiseRejectedHandle();
+        }
+    }
+    private reportTracker<T>(data: T) {
+        const params = Object.assign(this.data, data, { time: new Date().getTime() });
+        let headers = {
+            type: 'application/x-www-form-urlencoded'
+        };
+        let blob = new Blob([JSON.stringify(params)], headers);
+        navigator.sendBeacon(this.data.requestUrl, blob);
     }
 }
